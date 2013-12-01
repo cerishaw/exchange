e287faf249a4401992093b3af908a14f79239673
Ceri Shaw <mrs.cshaw@gmail.com>
2013-12-01 22:16:21 +0000

    set max date to yesterday

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,7 @@
 $(function() {
     $("#currency_conversion_date").datepicker({
         minDate: new Date($("#minDate").val()),
-        maxDate: new Date(),
+        maxDate: ((new Date()).getDate() - 2),
         dateFormat: 'dd/mm/yy'
     });
 })
