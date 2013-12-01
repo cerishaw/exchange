b3fdb7bca0abc1e646cc081a9592c75dd6ccd915
Ceri Shaw <mrs.cshaw@gmail.com>
2013-12-01 14:17:51 +0000

    calculate exchange rate from database

--- app/helpers/exchange_rate.rb
+++ app/helpers/exchange_rate.rb
@@ -1,5 +1,17 @@
 module ExchangeRate
-  def self.at(date, base_currency, counter_currency)
-    1.4
+  def self.at(date, base_currency_code, counter_currency_code)
+
+    base_rate = find_conversion_rate_on_date(date, base_currency_code)
+    counter_rate = find_conversion_rate_on_date(date, counter_currency_code)
+
+    counter_rate / base_rate
+  end
+
+  private
+
+  def self.find_conversion_rate_on_date(date, currency_code)
+    ConversionRate.joins('INNER JOIN currencies ON currencies.id = conversion_rates.currency_id')
+                    .where(currencies: {code: currency_code}, date: date)
+                    .first!.rate
   end
 end
