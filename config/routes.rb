6e546e9538d79672c628a9056cdd93fc9366b5e0
Ceri Shaw <mrs.cshaw@gmail.com>
2013-12-01 16:47:28 +0000

    add home route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Exchange::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'currency_conversions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
